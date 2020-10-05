14780613d9bbcd84db24f2f864a5aff984f6cad1
TNORTHERN <traynorthern@yahoo.com>
2020-10-04 22:12:41 -0500

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '96620a6a26c5734ce842df3470c2eaf8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8e9f95b3-7aa5-4a26-935a-f5936fc6d0ba'
+};
