72426f37e0783ae32b6af1665a868e09dbae4732
TNORTHERN <traynorthern@yahoo.com>
2021-01-31 11:49:58 -0600

    switch to aws for files

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,15 @@
 module.exports = ({ env }) => ({
+  // ...
   upload: {
     provider: "aws-s3",
     providerOptions: {
       accessKeyId: env("AWS_ACCESS_KEY_ID"),
       secretAccessKey: env("AWS_ACCESS_SECRET"),
-      endpoint: `${env("AWS_REGION_NAME")}.digitaloceanspaces.com`,
       region: env("AWS_REGION_NAME"),
       params: {
-        Bucket:env("AWS_BUCKET_NAME"),
+        Bucket: env("AWS_BUCKET_NAME"),
       },
     },
   },
+  // ...
 });
