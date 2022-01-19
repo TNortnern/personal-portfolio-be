6024c9bdb79bc4cafc0e8fdc6eff6412b545480c
tnorthern <traynorthern@yahoo.com>
2022-01-19 15:39:32 -0600

    update project structure

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,15 @@
 module.exports = ({ env }) => ({
   // ...
   upload: {
+    config: {
+      breakpoints: {
+        xlarge: 1600,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64,
+      },
+    },
     provider: "aws-s3",
     providerOptions: {
       accessKeyId: env("AWS_ACCESS_KEY_ID"),
